4bba941281977bdde1e468f1e3c75603decdaf62
CedricSanchezGithub <mycedmail@gmail.com>
2024-11-15 15:39:25 +0100

    spark.py

--- src/spark.py
+++ src/spark.py
@@ -1,10 +1,29 @@
-import requests
+from pyspark.sql import SparkSession
 
-url = "https://world.openfoodfacts.org/api/v0/product/737628064502.json"
-response = requests.get(url)
+spark = SparkSession.builder \
+    .appName("Analyse OpenFoodFacts") \
+    .getOrCreate()
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f"Erreur : {response.status_code}")
+file_path = "../datas/en.openfoodfacts.org.products.csv"
+df = spark.read.csv(file_path, header=True, inferSchema=True)
+
+# Afficher les noms des colonnes
+print("### Noms des colonnes :")
+print(df.columns)
+
+# Compter le nombre total de lignes
+total_lignes = df.count()
+print(f"### Nombre total de lignes : {total_lignes}")
+
+df.describe().show()
+
+parquet_file = "../datas/openfoodfact.parquet"
+df.write.parquet(parquet_file)
+
+print(f"Fichier Parquet sauvegardé à : {parquet_file}")
+
+df_parquet = spark.read.parquet(parquet_file)
+df_parquet.show()
+
+
+spark.stop()
